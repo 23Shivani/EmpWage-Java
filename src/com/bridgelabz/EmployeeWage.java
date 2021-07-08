25514a81a8e2b69797cede08c1c2177fc3452cd7
23Shivani <shivani.gharve.2310@gmail.com>
2021-07-08 17:35:15 +0530

    Added: Part Time Wage Calculation

--- src/com/bridgelabz/EmployeeWage.java
+++ src/com/bridgelabz/EmployeeWage.java
@@ -4,19 +4,23 @@ public class EmployeeWage {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int FULL_TIME = 1;
+		int PART_TIME = 1;
+        int FULL_TIME = 2;
         int WAGE_PER_HR = 20;
-        int empCheck = (int) (Math.random() * 100) % 2;
+        int empCheck = (int) (Math.random() * 100) % 3;
         int workingHours = 0;
         if (empCheck == FULL_TIME)
         {
-            System.out.println("Employee is Present");
+            System.out.println("Employee is Present Full time");
+            workingHours = 12;
+        } else if (empCheck == PART_TIME)
+        {
+            System.out.println("Employee is Present Part time");
             workingHours = 8;
         } else
         {
             System.out.println("Employee is Absent");
         }
-     
         int wage = workingHours * WAGE_PER_HR;
         System.out.println("Employee Daily Wage is " + wage);
     }
