41fad6dfe149aabfee785cb115f41c1863ef5971
23Shivani <shivani.gharve.2310@gmail.com>
2021-07-08 17:39:12 +0530

    Added: Switch Case Statement

--- src/com/bridgelabz/EmployeeWage.java
+++ src/com/bridgelabz/EmployeeWage.java
@@ -4,22 +4,23 @@ public class EmployeeWage {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int PART_TIME = 1;
-        int FULL_TIME = 2;
-        int WAGE_PER_HR = 20;
+		final int PART_TIME = 1;
+        final int FULL_TIME = 2;
+        final int WAGE_PER_HR = 20;
         int empCheck = (int) (Math.random() * 100) % 3;
-        int workingHours = 0;
-        if (empCheck == FULL_TIME)
+        int workingHours =0;
+        switch (empCheck)
         {
-            System.out.println("Employee is Present Full time");
-            workingHours = 12;
-        } else if (empCheck == PART_TIME)
-        {
-            System.out.println("Employee is Present Part time");
-            workingHours = 8;
-        } else
-        {
-            System.out.println("Employee is Absent");
+            case FULL_TIME:
+                System.out.println("Employee is Present Full time");
+                workingHours = 8;
+                break;
+            case PART_TIME:
+                System.out.println("Employee is Present Part time");
+                workingHours = 4;
+                break;
+            default:
+                System.out.println("Employee is Absent");    
         }
         int wage = workingHours * WAGE_PER_HR;
         System.out.println("Employee Daily Wage is " + wage);
