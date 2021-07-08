7278ae87fcb5bc59e5ad66176e59c4c7e9a58c6f
23Shivani <shivani.gharve.2310@gmail.com>
2021-07-08 16:29:45 +0530

    Added: Emp Attendance

--- src/com/bridgelabz/EmployeeWage.java
+++ src/com/bridgelabz/EmployeeWage.java
@@ -4,7 +4,11 @@ public class EmployeeWage {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		int FULL_TIME = 1;
+		int empCheck = (int) (Math.random() * 10) % 2;
+		if (empCheck == FULL_TIME)
+			System.out.println("Employee is Present");
+		else
+			System.out.println("Employee is Absent");
 	}
-
 }
